58372601cb490939f63cf0cdc6f9b93156826639
AcervantesA <alexandercervantes2004@gmail.com>
2023-08-26 10:12:41 -0500

    progrreso

--- javas.js
+++ javas.js
@@ -1,13 +1,17 @@
-document.getElementById("miFormulario").addEventListener("submit", function(event) {
+document.getElementById("miFormulario").addEventListener("submit", function (event) {
     event.preventDefault();
 
     // Obtener los valores del formulario
-    var nombre = document.getElementById("nombre").value;
-    var email = document.getElementById("email").value;
+    var nombre = document.getElementById("nombres").value;
+    var apellido = document.getElementById("apellidos").value;
+    var identificacion = document.getElementById("identidad").value;
+    var periodo = document.getElementById("periodo_lect").value;
 
     // Guardar los datos en el almacenamiento local
-    localStorage.setItem("nombre", nombre);
-    localStorage.setItem("email", email);
+    localStorage.setItem("nombres", nombre);
+    localStorage.setItem("apellidos", apellido);
+    localStorage.setItem("identidad", identificacion);
+    localStorage.setItem("periodo_lect", periodo);
 
     // Redireccionar a otra página
     window.location.href = "otra_pagina.html";
